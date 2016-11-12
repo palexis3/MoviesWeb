d9b6f4515b943d476892f37befac8853282a621b
palexis3 <alexispatrick77@gmail.com>
2016-11-11 16:12:05 -0800

    First commit

---
+++ entertainment_center.py
@@ -0,0 +1,11 @@
+import media
+
+toy_story = media.Movie("Toy story", "A story of a boy and his toys that come to life", "https://en.wikipedia.org/wiki/Toy_Story_4#/media/File:Toy_Story_4_poster.jpg","https://www.youtube.com/watch?v=cPNW-0CHV4Q")
+avatar = media.Movie("Avatar", "A marine on an alien planet", "https://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+school_of_rock = media.Movie("School of Rock", "Using rock music to learn", "https://en.wikipedia.org/wiki/File:School_of_Rock_Poster.jpg", "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
+ratatouille = media.Movie("Ratatouille", "A rat is a chef in Paris", "https://en.wikipedia.org/wiki/File:RatatouillePoster.jpg", "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+midnight_in_paris = media.Movie("Midnight in Paris", "Going back in time to meet authors", "https://en.wikipedia.org/wiki/File:Midnight_in_Paris_Poster.jpg", "https://www.youtube.com/watch?v=FAfR8omt-CY")
+hunger_games = media.Movie("Hunger Games", "A really real reality show", "https://en.wikipedia.org/wiki/The_Hunger_Games#/media/File:The_Hunger_Games_cover.jpg", "https://www.youtube.com/watch?v=4S9a5V9ODuY")
+
+#print(avatar.trailer_youtube_url)
+#avatar.show_trailer()
